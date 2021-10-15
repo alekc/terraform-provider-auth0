faf324743e8c97b497c7ed0b08b072a6d4bae7c5
Alexander Chernov <alexander@chernov.it>
2021-10-15 12:30:15 +0100

    moved github

--- auth0/resource_auth0_connection_test.go
+++ auth0/resource_auth0_connection_test.go
@@ -1061,6 +1061,8 @@ resource "auth0_connection" "linkedin" {
 		client_secret = "client_secret_update"
 		strategy_version = 2
 		scopes = [ "basic_profile", "profile" ]
+		set_user_root_attributes = "on_each_login"
+		non_persistent_attrs = ["foo", "bar"]
 	}
 }
 `, rand),
@@ -1076,21 +1078,21 @@ resource "auth0_connection" "linkedin" {
 	})
 }
 
-func TestAccConnectionGitHub(t *testing.T) {
-
+func TestAccConnection_GitHub(t *testing.T) {
 	rand := random.String(6)
-
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
+				// language=hcl
 				Config: random.Template(`
-
 resource "auth0_connection" "github" {
 	name = "Acceptance-Test-GitHub-{{.random}}"
-	options {
+	github {
 		client_id = "client-id"
 		client_secret = "client-secret"
+		set_user_root_attributes = "on_each_login"
+		non_persistent_attrs = ["foo", "bar"]
 		scopes = [ "email", "profile", "read_user", "follow", "public_repo", "repo", "repo_deployment", "repo_status",
 				   "delete_repo", "notifications", "gist", "read_repo_hook", "write_repo_hook", "admin_repo_hook",
 				   "read_org", "admin_org", "read_public_key", "write_public_key", "admin_public_key", "write_org"
@@ -1100,29 +1102,34 @@ resource "auth0_connection" "github" {
 `, rand),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					random.TestCheckResourceAttr("auth0_connection.github", "name", "Acceptance-Test-GitHub-{{.random}}", rand),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.client_id", "client-id"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.client_secret", "client-secret"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.#", "20"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.0", "admin_org"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.1", "admin_public_key"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.2", "admin_repo_hook"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.3", "delete_repo"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.4", "email"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.5", "follow"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.6", "gist"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.7", "notifications"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.8", "profile"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.9", "public_repo"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.10", "read_org"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.11", "read_public_key"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.12", "read_repo_hook"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.13", "read_user"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.14", "repo"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.15", "repo_deployment"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.16", "repo_status"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.17", "write_org"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.18", "write_public_key"),
-					resource.TestCheckResourceAttr("auth0_connection.github", "options.0.scopes.19", "write_repo_hook"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.client_id", "client-id"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.client_secret", "client-secret"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.#", "20"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.0", "admin_org"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.1", "admin_public_key"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.2", "admin_repo_hook"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.3", "delete_repo"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.4", "email"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.5", "follow"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.6", "gist"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.7", "notifications"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.8", "profile"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.9", "public_repo"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.10", "read_org"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.11", "read_public_key"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.12", "read_repo_hook"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.13", "read_user"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.14", "repo"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.15", "repo_deployment"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.16", "repo_status"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.17", "write_org"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.18", "write_public_key"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.scopes.19", "write_repo_hook"),
+					resource.TestCheckResourceAttr("auth0_connection.github", "github.0.set_user_root_attributes", "on_each_login"),
+					resource.TestCheckResourceAttr("auth0_connection.github",
+						"github.0.non_persistent_attrs.0", "bar"),
+					resource.TestCheckResourceAttr("auth0_connection.github",
+						"github.0.non_persistent_attrs.1", "foo"),
 				),
 			},
 		},
