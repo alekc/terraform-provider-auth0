738c8248fb0c396b2c317c4646bcf6d235348208
Michael Newton <mnewton@cirtual.com>
2021-11-26 17:12:52 -0500

    auth0_resource_server data source implementation and tests

--- auth0/provider.go
+++ auth0/provider.go
@@ -67,6 +67,10 @@ func init() {
 			"auth0_action":          newAction(),
 			"auth0_flow":            newFlow(),
 		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"auth0_resource_server": dataSourceResourceServer(),
+		},
+
 		ConfigureContextFunc: Configure,
 	}
 }
